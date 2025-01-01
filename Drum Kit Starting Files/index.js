2e2cb287b796a8f3d649a44141b9882d76fc6e68
Sijan Adhikari <adhikarisijan909@gmail.com>
2025-01-02 00:06:47 +0545

    drumkit project

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,7 @@
+for(let i=0;i<=document.querySelectorAll(".drum").length;i++){
+document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+    this.style.color="white";
+    // let audio=new Audio("sounds/tom-1.mp3");
+    // audio.play();
+});
+}
