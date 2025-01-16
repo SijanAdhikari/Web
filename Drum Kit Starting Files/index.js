ee8ea259a1b05edfc025b11246e883caddcb3d03
Sijan Adhikari <adhikarisijan909@gmail.com>
2025-01-17 00:35:40 +0545

    callback function

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,7 +1,53 @@
-for(let i=0;i<=document.querySelectorAll(".drum").length;i++){
-document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-    this.style.color="white";
-    // let audio=new Audio("sounds/tom-1.mp3");
-    // audio.play();
+let numberOfDrumButtons=document.querySelectorAll(".drum").length;
+for(let i=0;i<numberOfDrumButtons;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+        });
+    }
+
+document.addEventListener("keydown",function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
 });
+
+function makeSound(key){
+    switch(key){
+        case "w":
+            var tom1=new Audio("sounds/tom-1.mp3");
+            tom1.play();
+        case "a":
+            var tom2=new Audio("sounds/tom-2.mp3");
+            tom2.play();
+        case "s":
+            var tom3=new Audio("sounds/tom-3.mp3");
+            tom3.play();
+        case "d":
+            var tom4=new Audio("sounds/tom-4.mp3");
+            tom4.play();
+        case "j":
+            var snare=new Audio("sounds/snare.mp3");
+            snare.play();
+        
+        case "k":
+            var crash=new Audio("sounds/crash.mp3");
+            crash.play();
+        case "l":
+            var kick=new Audio("sounds/kick-bass.mp3");
+            kick.play();
+        break;
+
+    default: console.log(buttonInnerHTML);
+        
+    }
+}
+
+function buttonAnimation(currentkey){
+    var activeButton=document.querySelector("."+currentkey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100)
+
 }
