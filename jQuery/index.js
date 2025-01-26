b4e65997e46debb25c37316ac2553e1307124a1c
Sijan Adhikari <adhikarisijan909@gmail.com>
2025-01-27 00:45:23 +0545

    use of jQuery

---
+++ jQuery/index.js
@@ -0,0 +1,52 @@
+// for(let i=0;i<$("button").length;i++){
+//     $("button")[i].addEventListener("click",function(){
+        
+//         $("h1").css("color","red");
+//     })
+// }
+
+
+//Getting the attribute
+
+//console.log($("img").attr("src"));
+
+//Setting the attribute
+
+//$("a").attr("href","www.youtube.com");
+
+
+//Adding eventlistener using jQuery
+
+// $("h1").click(function(){
+//     $("h1").css("color","purple");
+// })
+
+//AND TO TRAVERSE THROUGH ALL THE BUTTONS WE DON'T NEED TO APPLY THE METHOD AT THE TOP OF THE PAGE INSTEAD,we don't neet to use for loop
+
+// $("button").click(function(){
+//     $("h1").css("color","purple");
+// })
+
+
+// $("input").keydown(function(event){
+//     console.log(event.key);
+// })
+
+// $(document).keypress(function(event){
+//     $("h1").text(event.key);
+// })
+
+// $("h1").on("mouseover",function(){
+//     $("h1").css("color","green");
+// })
+
+//  ADD NEW ELEMENT USING JQUERY
+
+// $("h1").before("<button>New</button>");
+
+
+//Website animatins with jQuery
+
+$("button").on("click",function(){
+    $("h1").slideUp().slideDown().animate({opacity:0.5});
+});
